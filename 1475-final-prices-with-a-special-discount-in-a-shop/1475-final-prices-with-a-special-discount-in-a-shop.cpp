3291bb0fb3bd9fa58b481989b811f1967a65da42
Raghvendra Singh Yadav <125951142+Raghvendra0701@users.noreply.github.com>
2026-09-13 14:45:35 +0530

    Time: 0 ms (100%), Space: 14.1 MB (55.41%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> ans;
+        int n=prices.size();
+        for(int i=0;i<n;i++){
+            bool found = false;
+            for(int j=i+1;j<n;j++){
+                if(prices[j] <= prices[i]){
+                    ans.push_back(prices[i]-prices[j]);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                ans.push_back(prices[i]);
+            }
+        }
+        return ans;
+        
+    }
+};
